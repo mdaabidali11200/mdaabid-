f31cd552fd900b60cdb5e5c99bc04f744b2a74f0
mdaabid11200 <mdaabidali11200@gmail.com>
2022-12-29 22:11:02 +0530

    29 december 2022

---
+++ aap.js
@@ -0,0 +1,21 @@
+
+
+ const fruits = ["Mango", "Apple", "Banana", "Orange"];
+document.getElementById("demo").innerHTML = fruits.toString();
+
+
+const fruit = ["Mango", "Apple", "Banana", "Orange"];
+document.getElementById("demo1").innerHTML = fruit.join("_")
+
+const cars = ["Saab", 
+              "Volvo", 
+              "BMW",];
+document.getElementById("demo2").innerHTML = cars.join("( ....  )");
+
+
+
+// const flower =["rose", "lotus", "sunflower", "merigold"];
+
+// const newflower = flower.slice(2);
+
+// document.getElementById("demo").innerHTML = newflower;
